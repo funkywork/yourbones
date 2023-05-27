a7bc509fcce7c7c9d5070f8b403a50d622f083a8
xvw <xavier.vdw@gmail.com>
2023-05-27 23:32:04 +0200

    Move to OCaml >= 5.0.0 and chores on repo

--- test/ppx_test/prelude.ml
+++ test/ppx_test/prelude.ml
@@ -1,6 +1,9 @@
-#use "topfind";;
-#require "yourbones";;
-#require "yourbones_ppx";;
-#install_printer Yourbones_common.Tez.pp_print;;
-open Yourbones_common;;
+#use "topfind"
 
+#require "yourbones"
+
+#require "yourbones_ppx"
+
+#install_printer Yourbones_common.Tez.pp_print
+
+open Yourbones_common

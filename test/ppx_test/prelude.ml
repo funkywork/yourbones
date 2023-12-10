87e5eb75913e1aea42698220b5899dfea123243b
mspwn <spawnmick@gmail.com>
2023-12-10 02:43:53 +0100

    Add a sad ocamlformat-ignore for `ppx/prelude`
    Co-authored-by: xvw <xaviervdw@gmail.com>

--- test/ppx_test/prelude.ml
+++ test/ppx_test/prelude.ml
@@ -1,6 +1,6 @@
-#use "topfind";;
-#require "yourbones";;
-#require "yourbones-ppx";;
-#install_printer Yourbones.Tez.pp_print;;
-#install_printer Yourbones.Address.pp;;
-open Yourbones;;
+#use "topfind" ;;
+#require "yourbones" ;;
+#require "yourbones-ppx" ;;
+#install_printer Yourbones.Tez.pp_print ;;
+#install_printer Yourbones.Address.pp ;;
+open Yourbones ;;
